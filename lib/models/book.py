fc870ce5abeaae44f44a3b3412aacbc157ce598c
omolo-j <omoloj135@gmail.com>
2024-06-12 12:46:38 +0300

    adding delete in book file

--- lib/models/book.py
+++ lib/models/book.py
@@ -31,6 +31,20 @@ class Book:
         conn.commit()
         conn.close()
 
+    def delete(self):
+        if not self.id:
+            return False
+        
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+
+        c.execute('''DELETE FROM books WHERE id=?''', (self.id,))
+
+        conn.commit()
+        conn.close()
+
+        return True
+
     @staticmethod
     def get_all():
         conn = sqlite3.connect(DB_PATH)
