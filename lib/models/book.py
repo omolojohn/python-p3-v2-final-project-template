a86109983f827e8ecc7aa1083486db176839a447
omolo-j <omoloj135@gmail.com>
2024-06-12 12:39:28 +0300

    creating class in the book file

---
+++ lib/models/book.py
@@ -0,0 +1,17 @@
+# lib/models/book.py
+import sqlite3
+from datetime import datetime
+
+DB_PATH = 'db/library.db'
+
+class Book:
+    def __init__(self, title, author, quantity, member_id=None, created_at=None, id=None):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.quantity = quantity
+        self.member_id = member_id
+        self.created_at = created_at if created_at else datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    def __repr__(self):
+        return f"<Book(id={self.id}, title='{self.title}', author='{self.author}')>"
